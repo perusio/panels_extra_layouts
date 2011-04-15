7eb43a0bf22dfb382255afef2d9bf503bc1f4337
António P. P. Almeida <appa@perusio.net>
2011-04-15 16:08:11 +0100

    * Added the 44/35/21 three column stacked layout.

---
+++ plugins/layouts/threecol_44_35_21_stacked/threecol-44-35-21-stacked.tpl.php
@@ -0,0 +1,37 @@
+<!-- -*-html-helper-*- -->
+<div class="panel-display panel-threecol-44-37-19-stacked clear-block" <?php if (!empty($css_id)) { print "id=\"$css_id\"";} ?>>
+
+  <div class="panel-panel line">
+    <div class="panel-panel unit left">
+      <div class="inside">
+        <?php print $content['left']; ?>
+      </div>
+    </div>
+
+    <div class="panel-panel unit right lastUnit">
+      <div class="panel-panel line">
+        <div class="panel-panel unit top-middle">
+          <div class="inside">
+            <?php print $content['top_middle']; ?>
+          </div>
+        </div>
+
+        <div class="panel-panel unit top-right lastUnit">
+          <div class="inside">
+            <?php print $content['top_right']; ?>
+          </div>
+        </div>
+      </div>
+
+      <div class="inside">
+        <?php print $content['right']; ?>
+      </div>
+    </div>
+  </div>
+
+  <div class="panel-panel footer">
+    <div class="inside">
+      <?php print $content['footer']; ?>
+    </div>
+  </div>
+</div>
